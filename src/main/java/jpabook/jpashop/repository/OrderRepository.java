e039b8fefa3f69ab03dbe97ab0e7c3b59cb69a20
dare <gdare1999@gmail.com>
2022-03-16 20:55:01 +0900

    Order Repository

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // user별 주문 동적쿼리로 find
+    // public List<Order> findAll(OrderSearch orderSearch) { ... }
+}
